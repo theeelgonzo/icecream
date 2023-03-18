ab484373738d10d056f47dfe4bdcced1b86bb9d1
Jordan Godwin <jordan.godwin13@gmail.com>
2023-03-17 22:57:06 -0500

    Create variables in repeatable and unique flavor sets, and dictionary for bowls

--- iceCream.py
+++ iceCream.py
@@ -11,3 +11,25 @@ bowls of vanilla, but not two bowls of blueberry in combination with any other
 flavors.
 
 How many possible combinations can be made?"""
+
+# define flavors via set
+repFlav = {"chocolate", "vanilla", "strawberry"}
+
+uniqueFlav = {"blueberry", "cookie dough"}
+
+# create dictionary for bowls, where bowl number is key and the value is empty
+# the value will be set to a flavor
+
+bowls = {
+        "bowlOne": "",
+        "bowlTwo": "",
+        "bowlThree": "",
+        "bowlFour": ""
+        }
+
+print(repFlav)
+print(uniqueFlav)
+x = bowls.keys()
+y = bowls.values()
+print(x)
+print(y)
