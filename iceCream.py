8353eac0d3af21faaf7dffec7f781815c233e25d
Jordan Godwin <jordan.godwin13@gmail.com>
2023-03-17 22:44:37 -0500

    Add main file. Describe problem in comment.

---
+++ iceCream.py
@@ -0,0 +1,13 @@
+"""You have four bowls of ice cream.
+Each bowl can only have one scoop.
+There are three repeatable flavors: chocolate, vanilla, strawberry.
+These flavors can be in any amount of the four bowls.
+BUT
+There are two unique flavors: cookie dough and blueberry.
+If any bowl has either one of these flavors, no other bowls can have that
+flavor.
+For instance, there can be an arrangement with one bowl of blueberry and three
+bowls of vanilla, but not two bowls of blueberry in combination with any other
+flavors.
+
+How many possible combinations can be made?"""
